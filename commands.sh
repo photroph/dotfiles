f4adce8874e4634d24e8dd810e783530672690ca
photroph <sh.g.h.r1994@gmail.com>
2026-07-01 17:34:04 +0900

    add command

--- commands.sh
+++ commands.sh
@@ -1,6 +1,7 @@
 aws sts get-caller-identity # Check AWS credentials
 aws secretsmanager delete-secret --secret-id {SECRET_ID} --force-delete-without-recovery --region {AWS_REGION} # WARNING: Delete immediately a secret in AWS Secrets Manager
 
+brew list --installed-on-request
 
 docker builder prune # Remove unused build cache
 
